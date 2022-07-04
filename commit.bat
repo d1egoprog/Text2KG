3e03dc6bfc8c65432bb7dab8a6eba7c657cd0876
Diego Rincon-Yanez <d1egoprog@users.noreply.github.com>
2022-07-04 12:56:34 +0200

    Delete commit.bat

--- commit.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-git pull
-git add *.*
-git rm --cached commit.bat
-git remote add text2kg https://github.com/d1egoprog/Text2KG.git
-git commit -m "Updating %DATE% on %TIME%"
-pause
-git push -u text2kg master
